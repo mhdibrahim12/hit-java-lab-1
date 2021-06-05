ad410517258bc831a9703852130aa9fbcd17221b
Muzammilsheriff <84589950+Muzammilsheriff@users.noreply.github.com>
2021-06-06 01:46:53 +0530

    java lab

---
+++ demo.java
@@ -0,0 +1,12 @@
+package fire;
+import java.util.Scanner;
+public class demo {
+
+	public static void main(String[] args) {
+		int year;
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Enter any Year");
+		scan.close
+	}
+
+}
